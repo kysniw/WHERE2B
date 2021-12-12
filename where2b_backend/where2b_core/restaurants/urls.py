80c46b7df6b9a2263bd815ef948eaf42371f10c2
Jakub <jakub.i816@gmail.com>
2021-12-12 17:52:31 +0100

    add endpoints for uploading restaurant photos

--- where2b_backend/where2b_core/restaurants/urls.py
+++ where2b_backend/where2b_core/restaurants/urls.py
@@ -7,7 +7,10 @@ from .views import (
 	TableViewSet,
 	ListUserRestaurantsViewSet,
 	OpeningHoursViewSet,
-	GetImageView
+	GetRestaurantPhotoImageView,
+	RestaurantPhotoViewSet,
+	UploadRestaurantPhotoView,
+	RestaurantPhotoViewSet,
 )
 
 router = routers.SimpleRouter()
@@ -16,8 +19,10 @@ router.register(r'restaurant', RestaurantViewSet, basename='restaurant')
 router.register(r'table', TableViewSet, basename='table')
 router.register(r'user-restaurants', ListUserRestaurantsViewSet, basename='user-restaurants')
 router.register(r'opening-hours', OpeningHoursViewSet, basename='opening-hours')
+router.register(r'restaurant-photo', RestaurantPhotoViewSet, basename='restaurant-photo')
 
 urlpatterns = [
 	path('', include(router.urls)),
-	path('restaurant-photo-image/<int:pk>', GetImageView.as_view(), name='restaurant-photo-image')
+	path('restaurant-photo-image/<int:pk>', GetRestaurantPhotoImageView.as_view(), name='restaurant-photo-image'),
+	path('restaurant-photo-upload/', UploadRestaurantPhotoView.as_view(), name='restaurant-photo-upload')
 ]
